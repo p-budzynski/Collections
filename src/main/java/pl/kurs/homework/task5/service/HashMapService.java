39b334ae92cafda8874272509dcf5b388a3672c4
p-budzynski <p.budzynski@op.pl>
2024-05-20 20:54:37 +0200

    .clone()

--- src/main/java/pl/kurs/homework/task5/service/HashMapService.java
+++ src/main/java/pl/kurs/homework/task5/service/HashMapService.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 public class HashMapService {
 
     public <K, V> HashMap<K, V> shallowCopyHashMap(HashMap<K, V> original) {
-        return new HashMap<>(original);
+        return (HashMap<K, V>) original.clone();
     }
 
     public <K, V> void fillHashMap(HashMap<K, V> destination, HashMap<K, V> source) {
